91532cfa4cd1dbf2838fe3422a7d522adad800ca
Yamila <capulloyam@hotmail.com>
2022-10-26 23:56:01 -0300

    Arreglo de desafio

--- desafio2.js
+++ desafio2.js
@@ -1,124 +1,98 @@
 const fs = require('fs')
 
 class Contenedor {
-    constructor(nombreProducto){
-        this.nombreProducto=nombreProducto
+    constructor(nombreArchivo) {
+        this.nombreArchivo = nombreArchivo
     }
-     async save(producto) {
-       
-        let productos= []
+    async save(producto) {
+
+        let productos = []
         try {
-            if (fs.existsSync(this.nombreProducto)) {
-                let data = await fs.promises.readFile(this.nombreProducto, "utf-8");
-                 productos = JSON.parse(data);
+            let data = await fs.promises.readFile(this.nombreArchivo, "utf-8");
+            productos = JSON.parse(data);
+            if (productos.length != 0) {
+
                 let id = producto.length + 1;
-                productos.id = id;
+                producto.id = id;
                 productos.push(producto);
-                await fs.promises.writeFile(this.nombreProducto, JSON.stringify(productos, null, 2))
-                return {
-                    status: "success",
-                    message: "Se creo correctamente el id."
-                }
+                fs.writeFileSync(this.nombreArchivo, JSON.stringify(productos))
+
             } else {
-                user.id = 1;
-                await fs.promises.writeFile(
-                    this.nombreProducto,
-                    JSON.stringify([producto], null, 2)
-                )
+                producto.id = 0;
+                productos.push(producto);
+                fs.writeFileSync(this.nombreArchivo, JSON.stringify(productos))
             }
-            return {
-                status: "success",
-                message: "Se creo correctamente el id."
-            }
+
         } catch (error) {
-            status: "error";
-            message: error.message
-        }
+            producto.id = 0;
+            productos.push(producto);
+            fs.writeFileSync(this.nombreArchivo, JSON.stringify(productos))
+        } return producto.id
     }
-     async getAll () {
+    async getAll() {
         try {
-            if (fs.existsSync(this.nombreProducto)) {
-                let data = await fs.promises.readFile(this.nombreProducto, "utf-8");
-                const productos = JSON.parse(data);
-                return {
-                    status: "success",
-                    productos: productos
-                }
-            } else {
 
-                status: "error";
-                message: "No se encontró ningun producto"
+            let data = await fs.promises.readFile(this.nombreArchivo, "utf-8");
+            const productos = JSON.parse(data);
+            return productos
 
-            }
         } catch (error) {
-            status: "error";
-            message: error.message
+            return null
         }
     }
-     async getById (id)  {
-        if (!id) {
-            status: "error";
-            message: "No existe ese id"
-        } if (fs.existsSync(this.nombreProducto)) {
-            let data = await fs.promises.readFile(this.nombreProducto, "utf-8");
+    async getById(id) {
+        try {
+            let data = await fs.promises.readFile(this.nombreArchivo, "utf-8");
             const productos = JSON.parse(data);
             let producto = productos.find((producto) => producto.id == id);
-            if (producto) {
-                return {
-                    status: "success",
-                    producto: producto
-                }
-            }
-            else {
-                status: "error";
-                message: "No se puede encontrar ese producto"
-            }
+            return producto
         }
-    }
-
-        async deleteById (id) {
-            if (!id) {
-                status: "error";
-                message: "No existe ese id"
-            }
-            if (fs.existsSync(this.nombreProducto)) {
-                let data = await fs.promises.readFile(this.nombreProducto, "utf-8");
-                const productos = JSON.parse(data);
-                let newProductos= productos.filter((producto)=> producto.id !=id);{
-                    await fs.promises.writeFile(
-                        this.nombreProducto,
-                        JSON.stringify(newProductos,null,2)
-                    );
-                    return{
-                        status:"success",
-                        message:"Producto eliminado correctamente",
-                    }                }
-            }
-        }
-        deleteAll() {
-            fs.writeFileSync(this.nombreProducto, "")
+        catch {
+            return null
         }
 
     }
-   
-   const nuevoNombre= new Contenedor('./productos.txt');
-   nuevoNombre.save({
-    tittle:"Sahumerios",
-    price:200,
-    thumbnail:"sahumerios.jpg",
-   },
-   {
-    tittle:"Velas",
-    price:300,
-    thumbnail:"velas.jpg",
-   },
-   {
-    tittle:"Barajas tarot",
-    price:2000,
-    thumbnail:"barajas.jpg",
-   })
-  // nuevoNombre.getById(3)
-   nuevoNombre.getAll()
+
+
+
+
+    async deleteById(id) {
+        try {
+            let data = await fs.promises.readFile(this.nombreArchivo, "utf-8");
+            const productos = JSON.parse(data);
+            let newProductos = productos.filter((producto) => producto.id != id); 
+            fs.writeFileSync(this.nombreArchivo,JSON.stringify(newProductos))
+        } catch {
+            return null
+        }
+
+
+    }
+    deleteAll() {
+        fs.writeFileSync(this.nombreArchivo, "")
+    }
+}
+
+
+    const nuevoNombre = new Contenedor('./productos.txt');
+nuevoNombre.save({
+        tittle: "Sahumerios",
+        price: 200,
+        thumbnail: "sahumerios.jpg",
+    }/* ,
+      {
+            tittle: "Velas",
+            price: 300,
+            thumbnail: "velas.jpg",
+        },
+        {
+            tittle: "Barajas tarot",
+            price: 2000,
+            thumbnail: "barajas.jpg",
+        } */).then(resolve=> {console.log(resolve)})
+// nuevoNombre.getById(3).then(resolve=> {console.log(resolve)})
+nuevoNombre.getAll().then(resolve=> {console.log(resolve)})
+
    //nuevoNombre.deleteById(2)
    //nuevoNombre.deleteAll()
 
